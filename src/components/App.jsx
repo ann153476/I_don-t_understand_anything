bacfb2a8494142c656f91bead1e4c646c9a7bac0
Anka <103377387+ann153476@users.noreply.github.com>
2023-02-19 22:53:53 +0200

    m

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,29 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from "react";
+
+import MyBooksBlock from "./MyBooksBlock/MyBooksBlock";
+
+class MyBooks extends Component {
+  render(){
+    return(
+      <>
+      <h1>sdfghjkl;</h1>
+      <MyBooksBlock>
+       
+      </MyBooksBlock>
+      <form>
+      <div>
+        <label>Book title</label>
+        <input placeholder="Book title" required />
+      </div>
+      <div>
+        <label>Book author</label>
+        <input placeholder="Book author" required />
+       </div>
+      <button type="submit">Add book</button>
+      </form>
+      </>
+    )
+  }
+}
+
+export default MyBooks;
---
+++ src/components/MyBooksBlock/MyBooksBlock.jsx
@@ -0,0 +1,12 @@
+
+const MyBooksBlock = () => {
+    return (
+        <div>
+            <h4>/////hjkl//////</h4>
+            
+        </div>
+    )
+}
+
+
+export default MyBooksBlock;
